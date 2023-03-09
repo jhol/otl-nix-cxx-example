386b297b5a4c6b5df0002e3ea164f20460f087f6
Joel Holdsworth <opentechlab@airwebreathe.org.uk>
2023-03-09 12:06:14 +0000

    Added docker image

--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgsUnstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs { inherit system; };
+        pkgsUnstable = import nixpkgsUnstable { inherit system; };
       in {
-        packages = {
+        packages = rec {
           default = pkgs.stdenv.mkDerivation {
             name = "otl_nix_test_app";
             src = ./.;
@@ -23,6 +25,11 @@
               boost
               SDL2
             ];
+          };
+
+          dockerImage = pkgsUnstable.dockerTools.buildNixShellImage {
+            tag = "latest";
+            drv = default.overrideAttrs (old: { src = null; });
           };
         };
       });
